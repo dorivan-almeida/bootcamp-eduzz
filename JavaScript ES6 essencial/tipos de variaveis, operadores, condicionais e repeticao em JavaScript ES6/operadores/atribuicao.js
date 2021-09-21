c8f8e35e3d514c2260093791ba8b0e1e82eccc15
Dorivan Almeida <dorivan.almeida@gmail.com>
2021-09-21 11:17:23 -0300

    estudo de operadores atribuicao.js

---
+++ JavaScript ES6 essencial/tipos de variaveis, operadores, condicionais e repeticao em JavaScript ES6/operadores/atribuicao.js
@@ -0,0 +1,24 @@
+// Atribuicao
+x = y
+
+// Atribuicao de adicao
+x = x + y // ou
+x += y
+
+// Atribuicao de subtracao
+x = x - y // ou
+x -= y
+
+// Atribuicao de multiplicacao
+x = x * y // ou
+x *= y
+
+// Atribuicao de divisao
+x = x / y // ou
+x /= y
+
+// Atribuicao de registo
+x = x % y // outro
+x %= y
+
+
