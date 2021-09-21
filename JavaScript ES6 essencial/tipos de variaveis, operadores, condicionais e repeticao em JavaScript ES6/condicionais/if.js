7c9cfeb9503c9f28a1ce539eb00af270dd9640b0
Dorivan Almeida <dorivan.almeida@gmail.com>
2021-09-21 15:06:26 -0300

    removendo pasta e arquivo

--- JavaScript ES6 essencial/tipos de variaveis, operadores, condicionais e repeticao em JavaScript ES6/condicionais/if.js
+++
@@ -1,8 +0,0 @@
-const array = [0,1,2,3,4,5];
-array.forEach (item => {
-  if (item % 2 === 0){
-    console.log(`O número ${item} é par.`);
-  }else{
-    console.log(`O número ${item} é ímpar.` );
-  }
-});
