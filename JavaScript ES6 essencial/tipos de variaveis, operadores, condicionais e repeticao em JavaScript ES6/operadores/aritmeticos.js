f8e353f455502926e798b8babaa2c5cd9a5999c6
Dorivan Almeida <dorivan.almeida@gmail.com>
2021-09-21 11:07:29 -0300

    estudo de operadores aritmeticos.js

---
+++ JavaScript ES6 essencial/tipos de variaveis, operadores, condicionais e repeticao em JavaScript ES6/operadores/aritmeticos.js
@@ -0,0 +1,38 @@
+// operador binario
+// operando1 operador operando2
+1 + 2
+
+// Operador unário
+// operando1 operador
+// operador operando1
+x++
+++x
+// =====================
+// Módulo (%)
+// Operador binário. Retorna o inteiro restante da revisão dos dois operadores
+12 % 5 // retorna 2
+
+// Incremento (++) e Decremento (--)
+++x
+x++
+const a = ++2 // já recebe o incrementado 3
+const a = 2++ // recebe o valor atual e depois é incrementado 3
+
+--x
+x--
+const a = --2 // já recebe o decrementado 1
+const a = 2-- // valor atual e depois decrementa 1
+
+// Negação (-) e Adição (+)
+-3
++"3"  //retorna 3 - força de string para numerico
++true // retorna 1
++false // retorna 0
+-true // retorna -1
+
+// Operador de exponenciação (**)
+2 ** 3 // retorno 8
+10 ** -1 // retorno 0.1
+
+// Operador de agrupamento ()
+2*(3+2)
