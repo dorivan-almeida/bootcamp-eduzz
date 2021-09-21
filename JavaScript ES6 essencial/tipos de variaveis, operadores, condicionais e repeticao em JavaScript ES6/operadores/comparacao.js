4932305e9db916d4a8cf5019610954e5bbec9f58
Dorivan Almeida <dorivan.almeida@gmail.com>
2021-09-21 11:40:41 -0300

    estudo de operadores comparacao.js

---
+++ JavaScript ES6 essencial/tipos de variaveis, operadores, condicionais e repeticao em JavaScript ES6/operadores/comparacao.js
@@ -0,0 +1,38 @@
+// Igual (==)
+// Retorna verdadeiro caso os operados sejam iguais. 3 == var1
+"3" == var1
+3 == '3' // true
+
+// Não igual (!=)
+// Retorna verdadeiro caso os operados não sejam iguais. var1 != 4
+var2 != "3"
+
+// Estritamente igual (===)
+// Retorna verdadeiro caso os operaores sejam iguais e do mesmo tipo. Veja também Object e igualdade
+3 === var1
+
+// Estritamente não igual (!==)
+// Retorna verdadeiro caso os operadores não sejam iguais e/ou não sejam do mesmo tipo.
+var1 !== "3"
+3 !== '3'
+
+
+// Maior que (>)
+// Retorna verdadeiro caso o operando de esquerda seja maior que o da direita.
+ var2 > var1
+ "12" > 2
+
+// Maior que (>=)
+// Retorna verdadeiro caso o operando de esquerda seja maior ou igual ao da direita.
+ var2 >= var1
+ var1 >= 3
+
+ // Maior que (<)
+// Retorna verdadeiro caso o operando de esquerda seja menor que o da direita.
+var2 < var1
+"12" < 2
+
+// Maior que (<=)
+// Retorna verdadeiro caso o operando de esquerda seja menor ou igual ao da direita.
+var2 <= var1
+var2 <= 5
